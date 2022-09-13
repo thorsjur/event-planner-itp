ebe205c5be9326277e7d593038c1e5b80c12b14c
Palina Yarmolenka <palinaya@stud.ntnu.no>
2022-09-13 11:17:33 +0000

    added app and controller class

--- project/src/main/java/project/App.java
+++ project/src/main/java/project/App.java
@@ -1,13 +1,25 @@
 package project;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("/App.fxml"));
+        Parent parent = fxmlLoader.load();
+        stage.setScene(new Scene(parent));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }
+
