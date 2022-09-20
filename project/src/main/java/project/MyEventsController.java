2799d89dc85b2692689cedc306120036ccd77b1f
chriveib <chriveib@stud.ntnu.no>
2022-09-20 12:02:21 +0200

    Renaming and adding seperate controllers

---
+++ project/src/main/java/project/MyEventsController.java
@@ -0,0 +1,32 @@
+package project;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class MyEventsController {
+    
+    @FXML
+    private Button CreateEventButton, EventsButton, MyEventsButton;
+
+    @FXML
+    private void handleMyEventsButtonClicked() {
+        
+    }
+
+    @FXML
+    private void handleEventsButtonClicked(){
+        
+    }
+
+    @FXML
+    private void handleCreateEventButtonClicked(){
+        
+    }
+}
+
