dc3d597fe82f2f4ef61aa4508a45bbbf941a0601
chriveib <chriveib@stud.ntnu.no>
2022-09-21 09:54:35 +0200

    Refactored controller-classes (#13)

--- project/src/main/java/project/AppController.java
+++ project/src/main/java/project/AppController.java
@@ -5,7 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 
-public class AllEventsController {
+public class AppController {
     
     @FXML
     private Button CreateEventButton, EventsButton, MyEventsButton;
