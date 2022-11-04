8e0233e9f2970d85167002e739d13aeb057ca2fc
Thor Sjursen <thor.sjursen@ntnu.no>
2022-11-04 13:07:41 +0100

    Improved code coverage for json (#100)
    Written improved tests for the writers and readers in the json layer,
    in the core module.

--- eventplanner/core/src/test/java/eventplanner/json/UserCollectionJsonWriterTest.java
+++ eventplanner/core/src/test/java/eventplanner/json/UserCollectionJsonWriterTest.java
@@ -1,18 +1,25 @@
 package eventplanner.json;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
 
+import eventplanner.core.Event;
 import eventplanner.core.User;
 
 @TestInstance(Lifecycle.PER_CLASS)
@@ -20,6 +27,8 @@ public class UserCollectionJsonWriterTest {
 
     public static final String FILE_PATH = "src/test/java/resources/data/user.json";
     private static final File FILE = new File(FILE_PATH);
+    private static final UserCollectionJsonWriter WRITER = new UserCollectionJsonWriter();
+    private static final UserCollectionJsonReader READER = new UserCollectionJsonReader();
 
     private Collection<User> actualUsers;
 
@@ -29,16 +38,20 @@ public class UserCollectionJsonWriterTest {
 
     @BeforeAll
     public void setup() throws IOException {
-        UserCollectionJsonWriter writer = new UserCollectionJsonWriter();
         expectedUsers.addAll(IOTestUtil.getPseudoRandomUsers(15));
-        writer.save(expectedUsers, FILE);
+        WRITER.save(expectedUsers, FILE);
     }
 
     @BeforeEach
     public void beforeEach() throws IOException {
-        UserCollectionJsonReader reader = new UserCollectionJsonReader();
-        actualUsers = reader.load(FILE);
+        actualUsers = READER.load(FILE);
     }
+
+    @AfterAll
+    public void tearDown() throws IOException {
+        WRITER.save(new ArrayList<User>(), FILE);
+    }
+
 
     @Test
     public void testSave_retainsUserData() {
@@ -46,5 +59,24 @@ public class UserCollectionJsonWriterTest {
         assertTrue(
                 expectedUsers.stream()
                         .allMatch(expected -> actualUsers.contains(expected)));
+    }
+
+    @ParameterizedTest
+    @NullSource
+    public void testLoad_doesNotThrowOnNullInput(File input) throws IOException {
+        Collection<User> users = READER.load(input);
+        assertDoesNotThrow(() -> WRITER.save(users, null));
+    }
+
+    @Test
+    public void testLoad_throwsFileNotFoundExceptionOnNonexistentFile() {
+        File nonExistentFile = new File("./ThisIsNotAFile.json");
+        assertThrows(FileNotFoundException.class, () -> WRITER.save(actualUsers, nonExistentFile));
+    }
+
+    @Test
+    public void testLoad_throwsOnInvalidFileType() throws IOException {
+        File file = File.createTempFile("tempfile", null);
+        assertThrows(IllegalArgumentException.class, () -> WRITER.save(actualUsers, file));
     }
 }
